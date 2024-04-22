94b60dae3e3d7565680d27e05160a93a979041f7
shijingNWPU <13027088562@163.com>
2024-04-22 17:18:03 +0800

    rust

--- src/sign.rs
+++ src/sign.rs
@@ -4,22 +4,29 @@ use super::utils::ffi;
 
 #[repr(C)]
 #[derive(Debug, Clone)]
-pub struct Message([u8; MLEN]);
+pub struct Message{
+    pub data: Vec<u8>,
+}
+//([u8; MLEN]);
 
 #[repr(C)]
 #[derive(Debug, Clone)]
 pub struct SignedMessage {
     pub smlen: usize,
-    pub signed_message_data: [u8; MLEN + CRYPTO_BYTES],
+    pub data: Vec<u8>,
 }
 
 #[repr(C)]
 #[derive(Debug, Clone)]
-pub struct PublicKey([u8; CRYPTO_PUBLICKEYBYTES]);
+pub struct PublicKey{
+    pub data: Vec<u8>,
+}
 
 #[repr(C)]
 #[derive(Debug, Clone)]
-pub struct SecretKey([u8; CRYPTO_SECRETKEYBYTES]);
+pub struct SecretKey{
+    data: Vec<u8>,
+}
 
 #[repr(C)]
 #[derive(Debug, Clone)]
@@ -29,14 +36,23 @@ pub struct KeyPair {
 }
 
 pub fn get_key_pair() -> Result<KeyPair, error::Error> {
-    let mut pk_data: [u8; CRYPTO_PUBLICKEYBYTES] = [0; CRYPTO_PUBLICKEYBYTES];
-    let mut sk_data: [u8; CRYPTO_SECRETKEYBYTES] = [0; CRYPTO_SECRETKEYBYTES];
+    let mut pk_data: Vec<u8> =  Vec::with_capacity(CRYPTO_PUBLICKEYBYTES);
+    let mut sk_data: Vec<u8> =  Vec::with_capacity(CRYPTO_SECRETKEYBYTES);
 
     unsafe {
         ffi::pqcrystals_dilithium2_ref_keypair(pk_data.as_mut_ptr(), sk_data.as_mut_ptr());
     }
-    let pk = PublicKey(pk_data);
-    let sk = SecretKey(sk_data);
+
+    let pk_data_output = unsafe {
+        std::slice::from_raw_parts(pk_data.as_ptr(), CRYPTO_PUBLICKEYBYTES).to_vec()
+    };
+    let sk_data_output = unsafe {
+        std::slice::from_raw_parts(sk_data.as_ptr(), CRYPTO_SECRETKEYBYTES).to_vec()
+    };
+
+    let pk = PublicKey{ data:pk_data_output };
+    let sk = SecretKey{ data:sk_data_output };
+    
     Ok(KeyPair {
         public_key: pk,
         secret_key: sk,
@@ -44,62 +60,79 @@ pub fn get_key_pair() -> Result<KeyPair, error::Error> {
 }
 
 pub fn generate_message() -> Result<Message, error::Error> {
-    let mut m: [u8; MLEN] = [0; MLEN];
+    let mut m: Vec<u8> =  Vec::with_capacity(MLEN);
+    //let mut m: [u8; MLEN] = [0; MLEN];
     unsafe {
         ffi::randombytes(m.as_mut_ptr(), MLEN);
     }
-    Ok(Message(m))
+
+    let random_data = m.as_ptr();
+
+    let message = unsafe {
+        std::slice::from_raw_parts(random_data, MLEN).to_vec()
+    };
+
+    println!("message:{:?}", message);
+    Ok(Message{
+        data: message,
+    })
 }
 
 pub fn sign(m: &Message, sk: &SecretKey) -> Result<SignedMessage, error::Error> {
-    let mut sm: [u8; MLEN + CRYPTO_BYTES] = [0; MLEN + CRYPTO_BYTES];
+    let mut sm: Vec<u8> =  Vec::with_capacity(m.data.len() + CRYPTO_BYTES);
     let mut smlen: usize = 0;
 
     unsafe {
         ffi::pqcrystals_dilithium2_ref(
             sm.as_mut_ptr(),
             &mut smlen as *mut usize,
-            m.0.as_ptr(),
-            MLEN,
-            sk.0.as_ptr(),
+            m.data.as_ptr(),
+            m.data.len() ,
+            sk.data.as_ptr(),
         );
         // println!("smlen:{:?}",smlen);
     }
+
+    let sm_data_out = sm.as_ptr();
+    let signed_message = unsafe {
+        std::slice::from_raw_parts(sm_data_out, smlen).to_vec()
+    };
+
     Ok(SignedMessage {
-        smlen,
-        signed_message_data: sm,
+        smlen: smlen,
+        data: signed_message,
     })
 }
 
 pub fn verify_sign(m: &Message, sm: &SignedMessage, pk: &PublicKey) -> Result<i32, error::Error> {
     let result;
-    let mut m2: [u8; MLEN + CRYPTO_BYTES] = [0; MLEN + CRYPTO_BYTES];
 
-    let mut mlen: usize = 0;
-    let smlen: usize = sm.smlen.clone();
+    let mut m2: Vec<u8> =  Vec::with_capacity(m.data.len() + CRYPTO_BYTES);
+    let mut m2len: usize = 0;
 
     unsafe {
         result = ffi::pqcrystals_dilithium2_ref_open(
             m2.as_mut_ptr(),
-            &mut mlen as *mut usize,
-            sm.signed_message_data.as_ptr(),
-            smlen,
-            pk.0.as_ptr(),
+            &mut m2len as *mut usize,
+            sm.data.as_ptr(),
+            sm.smlen,
+            pk.data.as_ptr(),
         )
     }
+
 
     if result == 1 {
         return Err(error::Error::FailedVerification);
     }
 
-    if mlen != MLEN {
-        println!("mlen:{:?} MLEN:{:?}", mlen, MLEN);
+    if m2len != m.data.len() {
+        println!("m2len:{:?} mlen:{:?}", m2len, m.data.len());
         return Err(error::Error::InvalidMessageLength);
     }
 
-    for j in 0..mlen {
-        if m.0[j] != m2[j] {
-            println!("m.0[j]:{:?} m2[j]:{:?}", m.0[j], m2[j]);
+    for (m_item, m2_item) in m.data.iter().zip(m2.iter()){
+        if m_item == m2_item {
+            println!("m.0[j]:{:?} m2[j]:{:?}", m_item, m2_item);
             return Err(error::Error::InvalidMatch);
         }
     }
--- src/tests.rs
+++ src/tests.rs
@@ -3,10 +3,14 @@ use super::sign;
 #[test]
 fn test_sign() {
     let message = sign::generate_message().unwrap();
-
+    println!("message:{:?}", message);
+    
     let key_pair = sign::get_key_pair().unwrap();
+    //println!("key_pair:{:?}", key_pair);
 
     let signed_message = sign::sign(&message, &key_pair.secret_key).unwrap();
+    //println!("signed_message:{:?}", signed_message);
 
-    sign::verify_sign(&message, &signed_message, &key_pair.public_key).unwrap();
+    let result = sign::verify_sign(&message, &signed_message, &key_pair.public_key).unwrap();
+    print!("result:{:?}", result);
 }
